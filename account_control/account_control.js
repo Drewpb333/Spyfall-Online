0f1c964eb0485d513c32fc1da9ffb8c6f973bdb1
Michael Gerov <mhgerov@gmail.com>
2018-03-29 12:06:10 -0400

    account creation successful, addition to roster successful

--- account_control/account_control.js
+++ account_control/account_control.js
@@ -8,3 +8,49 @@ var config = {
 	messagingSenderId: "1066986603676"
 };
 firebase.initializeApp(config);
+//var to hold auth object
+var auth = firebase.auth();
+var db = firebase.database();
+
+//pull roster from firebase db
+displayRoster();
+
+var user;
+$(document).ready(function() {
+	//click listener for sign-in button
+	$('form').submit(function(evt) {
+		//Prevent page refresh
+		evt.preventDefault();
+		//Firebase Anonymous Account Creation	
+		auth.signInAnonymously().catch(function(error) {
+			console.error(error.message);
+		});
+
+		//Once created, add to roster
+		auth.onAuthStateChanged(function(usr) {
+			if(usr) {
+				//Assign user's display name from form input
+				var dName = $('input:text').val().trim();
+				usr.updateProfile({displayName: dName});
+				user = usr;
+				console.log(user.displayName);	
+
+				//update roster
+				db.ref('roster/'+user.uid).set({uid: user.uid, displayName: user.displayName});
+				displayRoster();
+			}
+			else {
+				console.log('no user returned');
+			}
+		});
+	});
+});
+
+//Returns array of displayNames on roster
+function displayRoster() {
+	console.log(db.ref('roster/'));
+}
+
+function addUser() {
+	
+}
