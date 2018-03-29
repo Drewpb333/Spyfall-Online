26110206b7c667347615aaab158312d614180932
Michael Gerov <mhgerov@gmail.com>
2018-03-29 10:06:36 -0400

    init files

---
+++ account_control/account_control.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAGNVQ9YDyUNSl_7RNF10MmNzHEJHYtOJE",
+	authDomain: "spyfall-e6768.firebaseapp.com",
+	databaseURL: "https://spyfall-e6768.firebaseio.com",
+	projectId: "spyfall-e6768",
+	storageBucket: "spyfall-e6768.appspot.com",
+	messagingSenderId: "1066986603676"
+};
+firebase.initializeApp(config);
