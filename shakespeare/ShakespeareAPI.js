e88e371d13e5fc676d256e51b9389d581fa4f1e9
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-04-03 10:12:37 -0400

    Shakespeare API

---
+++ shakespeare/ShakespeareAPI.js
@@ -0,0 +1,18 @@
+$("#translate").on("click", "#translate-button", function (event) {
+    event.preventDefault();
+    var userInput = $("#user-input").val().trim();
+
+    // div where text will be appended
+    var textDiv = $("#translated-text");
+    //queryUrl with userInput variable.  No API key is necessary
+    var queryUrl =
+        "https://cors-anywhere.herokuapp.com/https://api.funtranslations.com/translate/shakespeare.json?text=" +
+        userInput;
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        var translation = response["contents"]["translated"];
+        textDiv.html("<p>" + translation + "</p>");
+    });
+})
